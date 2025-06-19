b3f83847a067a766478175b3b9a2a0159f739793
Yarin <64698378+Wolfity@users.noreply.github.com>
2025-06-19 16:33:58 +0200

    1.0.1 Version checker

--- src/main/kotlin/me/wolfity/fancycrates/FancyCrates.kt
+++ src/main/kotlin/me/wolfity/fancycrates/FancyCrates.kt
@@ -2,6 +2,7 @@ package me.wolfity.fancycrates
 
 import me.wolfity.developmentutil.ext.registerListener
 import me.wolfity.developmentutil.files.CustomConfig
+import me.wolfity.developmentutil.misc.UpdateChecker
 import me.wolfity.developmentutil.player.PlayerManager
 import me.wolfity.fancycrates.animation.AnimationRegistry
 import me.wolfity.fancycrates.commands.CrateCommands
@@ -21,8 +22,11 @@ lateinit var plugin: FancyCrates
 
 class FancyCrates : JavaPlugin() {
 
+    companion object {
+        const val RESOURCE_ID = 126171
+    }
+
     private lateinit var _crateAnimationRegistry: AnimationRegistry
-    private lateinit var _playerRegistry: PlayerManager
     private lateinit var _crateManager: CrateManager
     private lateinit var _playerStateManager: PlayerStateManager
 
@@ -40,11 +44,14 @@ class FancyCrates : JavaPlugin() {
     val crateManager: CrateManager
         get() = _crateManager
 
-    val playerRegistry: PlayerManager
-        get() = _playerRegistry
+    private lateinit var updateChecker: UpdateChecker
 
     override fun onEnable() {
         plugin = this
+
+        this.updateChecker = UpdateChecker(this, plugin.description.version, RESOURCE_ID)
+        updateCheck()
+
         loadFiles()
         DatabaseManager.init()
 
@@ -75,6 +82,7 @@ class FancyCrates : JavaPlugin() {
 
     private fun registerListeners() {
         CrateListeners().registerListener(this)
+        updateChecker.registerListener(this)
     }
 
     private fun registerCommands() {
@@ -100,6 +108,18 @@ class FancyCrates : JavaPlugin() {
 
             this.placeholderCrate = CustomConfig(this, "crates/placeholder.yml")
         }
+    }
 
+    private fun updateCheck() {
+        updateChecker.getVersion(
+            this,
+            RESOURCE_ID
+        ) { version ->
+            if (this.description.version == version) {
+                logger.info("There is not a new update available.");
+            } else {
+                logger.info("There is a new update available for ${plugin.description.name}");
+            }
+        }
     }
 }
