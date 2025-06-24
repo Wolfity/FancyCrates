246a373f3a3129e036c4f0e740e5d8a70d528a72
Yarin <64698378+Wolfity@users.noreply.github.com>
2025-06-24 16:28:55 +0200

    v1.2.1 allow  console giving  keys

--- src/main/kotlin/me/wolfity/fancycrates/commands/CrateCommands.kt
+++ src/main/kotlin/me/wolfity/fancycrates/commands/CrateCommands.kt
@@ -9,12 +9,15 @@ import me.wolfity.fancycrates.plugin
 import me.wolfity.fancycrates.util.toCrateItem
 import me.wolfity.fancycrates.util.toKeyItem
 import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Named
 import revxrsal.commands.annotation.Optional
 import revxrsal.commands.annotation.Range
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
+import revxrsal.commands.command.CommandActor
 
 class CrateCommands {
 
@@ -26,22 +29,26 @@ class CrateCommands {
     @Command("fancycrates givekey", "fancycrate givekey", "fc givekey")
     @CommandPermission(ADMIN_PERMISSION)
     fun onCrateKey(
-        sender: Player,
+        sender: BukkitCommandActor,
         @Named("crate") crateName: CrateParameter,
         @Named("amount") @Range(min = 1.0) amountOfKeys: Int,
         @Optional @Named("player") target: Player?
     ) {
-        val finalTarget = target ?: sender
+        val finalTarget = target ?: if (sender.isPlayer) (sender.sender() as Player) else null
+        if (finalTarget == null) {
+            sender.reply(style("<red>Invalid Receiver"))
+            return
+        }
 
         val crate = plugin.crateManager.getCrateById(crateName.key)
         if (crate == null) {
-            sender.sendStyled("<red>This crate does not exist!")
+            sender.reply(style("<red>This crate does not exist!"))
             return
         }
 
         val item = crate.toKeyItem(amountOfKeys)
 
-        sender.sendStyled("<green>Gave $amountOfKeys ${serialize(style(crate.displayName))} keys to ${finalTarget.name}")
+        sender.reply(style("<green>Gave $amountOfKeys ${serialize(style(crate.displayName))} keys to ${finalTarget.name}"))
         finalTarget.sendStyled("<green>Received $amountOfKeys ${crate.displayName} keys")
         finalTarget.inventory.addItem(item)
     }
